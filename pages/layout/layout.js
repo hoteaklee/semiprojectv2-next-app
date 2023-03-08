f4843815c06ba78d9af1764a13edea7b82b5f77f
hoteaklee <cc18ht@naver.com>
2023-03-08 17:55:52 +0900

    저장소 초기화 및 레이아웃 설정, 초기화면 표시

---
+++ pages/layout/layout.js
@@ -0,0 +1,27 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = ({children}) => {
+    return (
+        <html lang="ko">
+        <head>
+            <meta charSet="UTF-8"/>
+                <title>index</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                    <link rel="stylesheet" href="/css/normalize.css"/>
+                        <link rel="stylesheet" href="/css/main.css"/>
+                            <link rel="stylesheet" href="/css/project2.css"/>
+
+        </head>
+        <body>
+        <div id="wrapper">
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </div>
+        </body>
+        </html>
+    );
+};
+
+export default Layout;
