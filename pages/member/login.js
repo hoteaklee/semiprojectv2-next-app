b6eff88385f1268e9c344c3cdfb718ebecd2e066
hoteaklee <cc18ht@naver.com>
2023-03-08 18:02:49 +0900

    회원가입,로그인, 회원정보,게시판, 리스트 표시

---
+++ pages/member/login.js
@@ -0,0 +1,18 @@
+const Login = () => {
+    return (
+        <div>
+            <h2>로그인</h2>
+            <form name="login">
+                <div><label htmlFor="uid">아이디</label>
+                    <input type="text" name="uid" id="uid" /></div>
+                <div><label htmlFor="pwd">비밀번호</label>
+                    <input type="password" name="pwd" id="pwd" /></div>
+                <div><label></label>
+                    <button type="button">로그인</button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
