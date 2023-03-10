57ef50359fcce5c2ad83e426f8a6720791e1d8a8
hoteaklee <cc18ht@naver.com>
2023-03-10 10:44:40 +0900

    MariaDB용  Board 클래스 구현 2 - REST API board list

---
+++ models/MariaDB.js
@@ -0,0 +1,24 @@
+import mariadb from "mariadb";
+
+const dbconfig = {
+    host: process.env.MARIADB_HOST,
+    user: process.env.MARIADB_USER,
+    password: process.env.MARIADB_PWD,
+    database: process.env.MARIADB_DB
+}
+
+const Mariadb = {
+    makeConn: async () => {
+        try {
+            return  await mariadb.createConnection(dbconfig);
+        } catch (e) { console.log(e);}
+    },
+    closeConn: async (conn) => {
+        if (conn) {
+            try { await conn.close(); }
+            catch (e) { console.log(e); }
+        }
+    }
+}
+
+module.exports = Mariadb;
