d6a89b15d2e2bb7b14a88b8aca33ea66d3e594d4
hoteaklee <cc18ht@naver.com>
2023-03-13 17:42:47 +0900

    MariaDB Member 클래스 구현 1 - 회원가입

---
+++ models/Member.js
@@ -0,0 +1,71 @@
+//Member.js
+const Mariadb = require('../models/Mariadb');
+const mariadb = require("mariadb");
+
+let membersql = {
+    insertsql : 'insert into member ' +
+        ' (userid, passwd, name, email)' +
+        '  ?, ?, ?, ?)',
+
+    loginsql : ' select count(userid) cnt from member where userid = ? and passwd = ? ',
+
+    selectOne: ` select member mno, userid, name, email, `+
+                    ` date_format(regdate, "%Y-%m-%d %H:%i:%s") regdate ` +
+                    ` from member where userid = ? `
+}
+
+class Member{
+
+
+    // 생성자 정의 - 변수 초기화
+    // 즉, 매개변수로 전달된 값을 클래스 멤버변수에 대입함
+    constructor(userid, passwd, name, email) {
+        this.userid = userid;
+        this.passwd = passwd;
+        this.name = name;
+        this.email = email;
+    }
+    // 회원 정보 저장
+    async insert(){
+        // 데이터베이스 처리 - sungjuk 테이블에 insert
+        let conn = null;
+        let params = [this.userid, this.passwd, this.name, this.email];
+        let result = -1;
+
+        try {
+            conn = await mariadb.makeConn()
+            result = await conn.query(membersql.insertsql, params);
+            await conn.commit();
+            if (result.affectedRows > 0) result = result.affectedRows;
+        } catch (ex) {console.log(ex);}
+        finally { await mariadb.closeConn(conn);}
+    }
+
+    async login (uid, passwd) {     // 로그인 처리
+        let conn = null;
+        let params = [uid, passwd];
+        let result= -1;
+
+        try { conn = await mariadb.makeConn();
+            result = await conn.query(membersql.loginsql, params);
+
+        } catch (e){ console.log(e); }
+        finally { await mariadb.closeConn(); }
+        return result;
+    }
+
+    async selectOne (uid) {     // 아이디로 겸색된 회원의 모든 정보 조회
+        let conn = null;
+        let params = [uid];
+        result = -1;
+
+        try { conn = await mariadb.makeConn();
+            result = await conn.query(membersql.selectOne, params);
+
+        } catch (e){ console.log(e); }
+        finally { await mariadb.closeConn(); }
+        return result;
+    }
+};
+
+module.exports = Member;
---
+++ pages/api/member/join.js
@@ -0,0 +1,17 @@
+import Member from "../member";
+
+export default async (req,res) => {
+    const {userid, passwd, name, email} = req.body;
+    // console.log(title, userid, contents);
+
+    try {
+        const cnt = new Member(userid, passwd, name, email).insert()
+            .then(result=>result);
+        //new Board(null,title,userid,null,contents,null).insert
+
+        console.log(await cnt);
+        res.status(200).json({cnt:await cnt});
+    } catch (err){
+        res.status(500).json(err);
+    }
+}
